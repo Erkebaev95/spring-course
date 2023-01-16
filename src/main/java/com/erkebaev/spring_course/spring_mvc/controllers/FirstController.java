31353341a76cdf7989c476446ab93c7334fbcd4a
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2023-01-16 20:59:32 +0600

    Work with interface model

--- src/main/java/com/erkebaev/spring_course/spring_mvc/controllers/FirstController.java
+++ src/main/java/com/erkebaev/spring_course/spring_mvc/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.erkebaev.spring_course.spring_mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,11 +25,14 @@ public class FirstController {
 
     // Get запрос через аннотацию @RequestParam
     // required = false мы можем обращаться к страницу без параметров
+    // Model - получаем доступа к модели в контроллере
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        //System.out.println("Hello, " + name + " " + surname);
 
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
