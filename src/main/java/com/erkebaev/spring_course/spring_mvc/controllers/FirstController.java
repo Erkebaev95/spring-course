e3c4c97610dcdafed76a6f05ad1bfe1cda2c2cdb
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2023-01-16 20:22:21 +0600

    Work with Get request and parameters

--- src/main/java/com/erkebaev/spring_course/spring_mvc/controllers/FirstController.java
+++ src/main/java/com/erkebaev/spring_course/spring_mvc/controllers/FirstController.java
@@ -3,13 +3,32 @@ package com.erkebaev.spring_course.spring_mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
+    // Get запрос через HttpServletRequest
+    /*@GetMapping("/hello")
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
+        return "first/hello";
+    }*/
+
+    // Get запрос через аннотацию @RequestParam
+    // required = false мы можем обращаться к страницу без параметров
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
