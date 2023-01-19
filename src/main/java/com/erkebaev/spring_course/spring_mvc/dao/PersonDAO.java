9a5d3239c5c20d0ab9155796472dee5ae25d3ba9
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2023-01-19 23:17:11 +0600

    CRUD: create two methods index and show.

---
+++ src/main/java/com/erkebaev/spring_course/spring_mvc/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package com.erkebaev.spring_course.spring_mvc.dao;
+
+import com.erkebaev.spring_course.spring_mvc.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Jack"));
+        people.add(new Person(++PEOPLE_COUNT, "Sergey"));
+        people.add(new Person(++PEOPLE_COUNT, "Ivan"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId()==id)
+                .findAny()
+                .orElse(null);
+    }
+}
